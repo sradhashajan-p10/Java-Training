ff878b8267ca04ea84dfce3b04faf3922e3c67e5
sradhashajan-P10 <sradha.shajan@people10.com>
2026-05-14 17:22:27 +0530

    Week 5 Demo

--- Week 3 Demo /RoomBookingApp/src/main/java/Booking/example/RoomBookingApp/BookingRepo.java
+++
@@ -1,9 +0,0 @@
-package Booking.example.RoomBookingApp;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookingRepo extends JpaRepository<Customer, Integer> {
-
-}
